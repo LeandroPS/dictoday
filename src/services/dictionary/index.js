ff48180e8c7f35d115b9fdda8b8d7ffdb1653f54
Leandro Pires de Souza <lpsouza@HUMAC79.local>
2023-10-12 20:54:57 -0300

    feat: add service to get dummy word of the day

---
+++ src/services/dictionary/index.js
@@ -0,0 +1,12 @@
+
+const getWordOftheDay = () => {
+    return {
+        word: "E.fê.me.ro",
+        wordClass: "adj",
+        definitions: ["De curta duração, que é breve, transitório", "Característica do que é temporário, momentâneo", "Tipo de flor que desabrocha e murcha no espaço de um dia"]
+    }
+}
+
+export {
+    getWordOftheDay
+}
